b7e2e41eb87767529c58a94cd6d7d54b03625d25
scolymushisp <36168270+MMolBus@users.noreply.github.com>
2022-11-22 19:21:46 +0100

    remove nas

--- R/descriptor.calculation.fun.R
+++ R/descriptor.calculation.fun.R
@@ -1,46 +1,46 @@
-# x = numeric vector object 
+# x = numeric vector object
 # descriptores.= character vector with descriptors that you whant to calculate over vector x
 # "median", "mean", "sd", "min", "max", "diff.range"
 
 descriptor.fun <- function(x, des) {
-  
+
   possible_descrip <- c("median", "mean","sd", "min", "max", "diff.range"
                         # "n.cell"
                         )
-  
+
   res_l <- list()
-  
+
   res_l[[1]]       <- length(x)
-   
-  
+
+
   if(any(unique(grepl(possible_descrip[1], des)))== TRUE){
-    res_l[[2]]       <- median(x)
+    res_l[[2]]       <- median(x, na.rm = T)
     }else{res_l[[2]] <- NULL}
-  
+
   if(any(unique(grepl(possible_descrip[2], des)))== TRUE){
-    res_l[[3]]       <- mean(x)
+    res_l[[3]]       <- mean(x, na.rm = T)
     }else{res_l[[3]] <- NULL}
-  
+
   if(any(unique(grepl(possible_descrip[3], des)))== TRUE){
-    res_l[[4]]       <- sd(x)
+    res_l[[4]]       <- sd(x, na.rm = T)
     }else{res_l[[4]] <- NULL}
-  
+
   if(any(unique(grepl(possible_descrip[4], des)))== TRUE){
-    res_l[[5]]       <- min(x)
+    res_l[[5]]       <- min(x, na.rm = T)
     }else{res_l[[5]] <- NULL}
-  
+
   if(any(unique(grepl(possible_descrip[5], des)))== TRUE){
-    res_l[[6]]       <- max(x)
+    res_l[[6]]       <- max(x, na.rm = T)
     }else{res_l[[6]] <- NULL}
-  
+
   if(any(unique(grepl(possible_descrip[6], des)))== TRUE){
-    res_l[[7]]       <- diff(range(x))
+    res_l[[7]]       <- diff(range(x, na.rm = T))
     }else{res_l[[7]] <- NULL}
-  
+
   res_l        <- plyr::compact(res_l)
   names(res_l) <- c("ncell",des)
-  
+
   res          <- unlist(res_l)
   return(res)
-  
+
 }
