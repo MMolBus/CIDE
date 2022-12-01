d7e59a0f0777c5e1f1a97602bf87a147326bbb8e
scolymushisp <36168270+MMolBus@users.noreply.github.com>
2022-12-01 17:57:09 +0100

    add xrite to mapir conversor

---
+++ R/xrite2mapir.R
@@ -0,0 +1,14 @@
+xrite_grey <- c(96.539, 81.257, 66.766, 50.867, 35.656, 20.461)
+xrite_grey_ran <- max(xrite_grey)-min(xrite_grey)
+xrite_grey_prop <- xrite_grey-min(xrite_grey)
+xrite_grey_perc <- (xrite_grey_prop/max(xrite_grey_prop))*100
+
+mapir_grey <-  c(83, 27, 21, 2)
+mapir_grey_ran <- max(mapir_grey)-min(mapir_grey)
+mapir_grey_prop <- mapir_grey-min(mapir_grey)
+mapir_grey_perc <- (mapir_grey_prop/max(mapir_grey_prop))*100
+
+
+
+
+
